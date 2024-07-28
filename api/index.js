e0fff2eb44dd5c8dc62bde70a817b15dc028a1e7
Nishant <npkjha02@gmail.com>
2024-07-28 19:59:00 +0530

    First commit

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
- import mongoose from 'mongoose';
- import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+// import cors from 'cors';
 
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -14,19 +20,34 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 
-  const app = express();
-  app.use(express.json());
+const app = express();
 
+// app.use(cors({
+//   origin: process.env.CROSS_ORIGIN,
+//   credentials: true
+// }))
 
+app.use(express.json());
+app.use(cookieParser());
 
- app.use('/api/user', userRoutes);
- app.use('/api/auth', authRoutes);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
- app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({
@@ -34,11 +55,4 @@ mongoose
     statusCode,
     message,
   });
-});
-
-
-
-
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
 });
